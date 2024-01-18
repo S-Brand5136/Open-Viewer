34ac07ca250dac1a2bdcc2bb417c29e946c32893
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2024-01-17 19:02:09 -0800

    update build-storybook in json params

---
+++ storybook-static/assets/preview-GGywM9Ic.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-99_jL6pS.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-BVkgF34h.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BVkgF34h.js","./iframe-99_jL6pS.js","./index-XiNr8FW2.js","./_commonjsHelpers-5-cIlDoe.js","./react-18-8u3SsdfO.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./_getPrototype-4FkL748a.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
