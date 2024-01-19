cf60168432bdea7f6bc14570deafb7bca14bec83
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2024-01-19 13:45:00 -0800

    build stories

---
+++ docs/assets/BaseControls.stories-tLcCR59l.js
@@ -0,0 +1,3 @@
+import{j as c}from"./jsx-runtime-AgcCsxC8.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const s=a=>c.jsx("div",{});try{s.displayName="BaseControls",s.__docgenInfo={description:"",displayName:"BaseControls",props:{}}}catch{}const m={title:"Controls/BaseControls",component:s,parameters:{layout:"fullscreen"},tags:["autodocs"],argTypes:{}},r={args:{}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {}
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const d=["Basic"];export{r as Basic,d as __namedExportsOrder,m as default};
